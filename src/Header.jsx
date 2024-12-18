135686623f8d4af4bd9d638005c1276e3ec0d2a9
Yathish Acharya <yathishkumar2013@gmail.com>
2024-12-19 02:15:13 +0530

    homepage changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,9 +6,7 @@ export default function Header() {
         <nav className="bg-green-600 text-white p-4">
             <ul className="flex justify-around">
                 <li><Link to="/" className="text-lg hover:underline">Home</Link></li>
-                <li><Link to="/network" className="text-lg hover:underline">Network</Link></li>
-                <li><Link to="/login" className="text-lg hover:underline">Login</Link></li>
-                <li><Link to="/profile" className="text-lg hover:underline">Profile</Link></li>
+                <li><Link to="/admin" className="text-lg hover:underline">Admin</Link></li>
             </ul>
         </nav>
     )
