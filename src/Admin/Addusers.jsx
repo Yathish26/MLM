8837542e1ddb7675d489c6b753a56403f222a3b6
Yathish Acharya <yathishkumar2013@gmail.com>
2025-01-03 10:48:20 +0530

    Added Bulk Button

--- src/Admin/Addusers.jsx
+++ src/Admin/Addusers.jsx
@@ -205,6 +205,11 @@ export default function Addusers() {
                     Data Sheet
                 </button>
             </Link>
+            <Link to={'/test/add'}>
+                <button className="w-52 px-4 py-2 mt-4 text-white bg-orange-500 rounded-lg hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-400">
+                    Bulk Add
+                </button>
+            </Link>
         </div>
     );
 }
